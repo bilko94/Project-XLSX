1e1d21a672d35962cd0add4c94eb2fd1db836eaf
Liam Krieling (Group) <liam.krieling@global.ntt>
2020-02-06 19:10:10 +0200

    added file reader to route

--- routes/load_data.js
+++ routes/load_data.js
@@ -1,6 +1,32 @@
 const router = require('express').Router();
-const xlsmodels = require('./xls_schema');
+const xlsmodels = require('../../project xls/models/xls_schema.js');
+const xldocreader = require('xlsx');
+const path = require('path');
+const fs = require('fs');
 
-router.route("/load_data").post((req,res) => {
-    
+router.post('/load_data', (req,res) => {
+    console.log(req.body);
+    var ret = filereader(req.body.dir);
+    console.log(ret);
+    ret.then(result => {
+        res.json(result);
+    })
 })
+
+function filereader(dir){
+    const dirpath = path.join(__dirname,dir);
+    var arr = [];
+    let promise = new Promise(resolve => {
+        fs.readdir(dirpath, function (err, files) {
+            if (err) {
+                return console.log('Unable to scan directory: ' + err);
+            }
+            files.forEach(function (file) {
+                arr.push(file);
+            });
+            resolve(files);
+        });
+    })
+    return (promise);
+}
+module.exports = router;
