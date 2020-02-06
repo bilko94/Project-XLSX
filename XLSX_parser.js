0fe1036b7f75ef3402b2c81153ba3dfdbd738230
root <root@SHANEOLIVARI.af.didata.local>
2020-02-06 19:13:30 +0200

    pulling

--- XLSX_parser.js
+++ XLSX_parser.js
@@ -1,11 +1,14 @@
 //This file just takes an xlsx file and converts it into an obj with a format.
 
-Parser();
+var data = Parser();
+// console.log(data);
 
 function Parser(){
     var XLSX = require('xlsx');
     var workbook = XLSX.readFile('./Excel files/Shane Skills.xlsx');
     var sheet_name_list = workbook.SheetNames;
+    var array = [];
+    //break the file into sheets and format each one seperately
     sheet_name_list.forEach(function(y) {
         var worksheet = workbook.Sheets[y];
         var headers = {};
@@ -14,7 +17,6 @@ function Parser(){
         for (a in worksheet) {
             if (a[0] === '!')  continue;
             var tt = 0;
-            console.log(a);
             for (var i = 0; i < a.length; i++) {
                 if (!isNaN(a[i])) {
                     tt = i;
@@ -36,6 +38,14 @@ function Parser(){
         data.shift();
         obj.sheet = y;
         obj.data = data;
-        // console.log(obj);
+        //push each sheet to an array before restarting the foreach loop
+        array.push(obj);
     })
+    var obj1 = {};
+    var obj2 = {};
+    var obj3 = {};
+    var obj4 = {};
+    
+    console.log(array);
+    // return(res);
 }
