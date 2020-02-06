576dbdb4a44c93d327622a68e6505bef6545697f
root <root@SHANEOLIVARI.af.didata.local>
2020-02-06 19:28:39 +0200

    pulling

--- XLSX_parser.js
+++ XLSX_parser.js
@@ -7,7 +7,7 @@ function Parser(){
     var XLSX = require('xlsx');
     var workbook = XLSX.readFile('./Excel files/Shane Skills.xlsx');
     var sheet_name_list = workbook.SheetNames;
-    var array = [];
+    var ret = {};
     //break the file into sheets and format each one seperately
     sheet_name_list.forEach(function(y) {
         var worksheet = workbook.Sheets[y];
@@ -36,16 +36,9 @@ function Parser(){
         }
         data.shift();
         data.shift();
-        obj.sheet = y;
-        obj.data = data;
-        //push each sheet to an array before restarting the foreach loop
-        array.push(obj);
+        ret[y] = data;
+        // console.log(ret);
     })
-    var obj1 = {};
-    var obj2 = {};
-    var obj3 = {};
-    var obj4 = {};
-    
-    console.log(array);
+    console.log(ret);
     // return(res);
 }
